f9a67257d3dee42f84bf490f30440dcf9302a474
MEENAKSHI <168412587+Pattanwalemeenakshi@users.noreply.github.com>
2026-02-24 13:19:54 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,7 +3,7 @@ class Hello
 	public static void main(String[] args)
 	{
 		System.out.println("hello world");
-		System.out.println("is hanged one");
+		System.out.println("this is hanged one");
 	}
 }
 
